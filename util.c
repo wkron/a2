dd24acc623dfc109e8d6025abe52db6a9abf69b4
William Kronbæk <wkronbaek@gmail.com>
2022-12-10 14:11:14 +0100

    knn virker vist nu

--- util.c
+++ util.c
@@ -7,50 +7,47 @@
 double distance(int d, const double *x, const double *y) {
   double dist = 0.0;
   for (int i = 0; i < d; i++){
-    dist += pow(x[i] + y[i],2.0);
+    dist += pow(x[i] - y[i],2.0);
   }
   dist = sqrt(dist);
   return dist;
 }
 
-
-int insert_if_closer(int k, int d,
-                     const double *points, int *closest, const double *query,
-                     int candidate) {
-  // Check if any points are not filled, if then put candidate there.
-  for (int i = 0; i < k; i++){
-    if (closest[i] == -1){
-      closest[i] = candidate;
-      return 1;
-    } 
-  }
-
-  //Calculate distance for all k points in closest.
-  double distances[k];
-  for (int j = 0; j < k; j++){
-    distances[j] = distance(d, query, &(points[closest[j]*d]));
-  }
-  if(candidate == 347){
-  printf("%f %f : \n", query[0], query[1]);
-  printf("%f %f %f %f %f \n", distances[0], distances[1], distances[2], distances[3], distances[4]);}
-  double largest_dist = 0.0;
-  int index_large = 0;
+int insert_if_closer(int k, int d, const double *points, int *closest, const double *query, int candidate) {
+  double candidate_distance = distance(d, query, &(points[candidate*d]));
+  //Check in order if closer than any in closest, if then put there.
   for (int l = 0; l < k; l++){
-    if (distances[l]>largest_dist){
-      largest_dist = distances[l];
-      index_large = l;
+    double dist = 0.0;
+    if (closest[l] == -1) {
+       dist = INFINITY;
+    } else {
+      dist = distance(d, query, &(points[closest[l]*d]));
+    }
+    if (dist>candidate_distance) {
+      closest[l] = candidate; 
+      break;
     } 
-  }
-
-  double candidate_distance = distance(d, query, &points[candidate*d]);
-  if(candidate == 347){
-    printf("candidate has dist %f", candidate_distance);
-    printf("no %d is %f. \n ", index_large+1, largest_dist);
-  }
-
-  if (largest_dist > candidate_distance){
-    closest[index_large] = candidate;
-    return 1;
   }
   return 0;
 }
+
+
+
+// det her er bare noget rod / mislykket kode
+
+  // Check if any points are not filled, if then put candidate there.
+  // for (int i = 0; i < k; i++){
+  //   if (closest[i] == -1){
+  //     closest[i] = candidate;
+  //     return 1;
+  //   } 
+  // }
+
+  //Calculate distance for all k points in closest.
+  // double distances[k];
+  // for (int j = 0; j < k; j++){
+  //   distances[j] = distance(d, query, &(points[closest[j]*d]));
+  // }
+
+        // printf("%f is smaller than %f. Swapping in index %i! \n", candidate_distance, distances[l], candidate);
+      // printf("The points are %f, %f and %f, %f. \n", points[candidate*d],  points[candidate*d+1],points[closest[l]*d], points[closest[l]*d+1]);
